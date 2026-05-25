5670defd4b0b05a32fc8ea53a9d3cf468d779848
venajustin <venajustin@gmail.com>
2026-05-25 12:27:42 -0700

    controls on ui, hiding of croshair and accelleration based movement on ground

---
+++ scenes/ui.gd
@@ -0,0 +1,30 @@
+extends Control
+
+@onready var primary_cont = $Control/PrimaryControls
+@onready var secondary_cont = $Control/SecondaryControls
+@onready var crosshair = $Croshair
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var player = get_tree().get_first_node_in_group("Player")
+	player.connect("display_crosshair", on_display_crosshair)
+	player.connect("set_controls_mode", on_set_controls_mode)
+	secondary_cont.visible = false
+	crosshair.visible = false
+
+func set_primary():
+	secondary_cont.visible = false
+	primary_cont.visible = true
+
+func set_secondary():
+	secondary_cont.visible = true
+	primary_cont.visible = false
+
+func on_display_crosshair(yesorno:bool):
+	crosshair.visible = yesorno
+	
+func on_set_controls_mode(primary:bool):
+	if primary:
+		set_primary()
+	else:
+		set_secondary()
